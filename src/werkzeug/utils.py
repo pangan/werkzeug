db3c14cedf721011d08c6615e766b9501fc41d31
Amir <pangan@gmail.com>
2023-11-20 09:25:19 +0000

    debug redirect

--- src/werkzeug/utils.py
+++ src/werkzeug/utils.py
@@ -276,7 +276,7 @@ def redirect(
         "<html lang=en>\n"
         "<title>Redirecting...</title>\n"
         "<h1>Redirecting...</h1>\n"
-        f'calling method: {calframe}'
+        #f'calling method: {calframe}'
         "<p>You should be redirected automatically to the target URL: "
         f'<a href="{html_location}">{html_location}</a>. If not, click the link.\n',
         code,
